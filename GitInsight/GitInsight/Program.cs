8cf46391ccadb9b41fb4650bec081d181a3578e4
LocatedInSpace <48300341+LocatedInSpace@users.noreply.github.com>
2022-12-04 21:41:00 +0100

    week one, done

--- GitInsight/GitInsight/Program.cs
+++ GitInsight/GitInsight/Program.cs
@@ -1,3 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LibGit2Sharp;
 
-Console.WriteLine("Hello, World!");
+namespace GitInsight
+{
+    enum Mode
+    {
+        CommitFrequency,
+        CommitAuthor,
+        Unknown
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parse the command-line arguments
+            var mode = ParseArgs(args);
+
+            // Get the path to the Git repository
+            var path = args[0];
+
+            // Open the repository
+            var repo = new Repository(path);
+
+            switch (mode)
+            {
+                case Mode.CommitFrequency:
+                    // group commits by their date, and then order by their grouped date
+                    var commitsByDate = repo.Commits
+                        .GroupBy(c => c.Author.When.Date)
+                        .OrderBy(g => g.Key);
+
+                    // Print the number of commits per day
+                    foreach (var group in commitsByDate)
+                    {
+                        Console.WriteLine($"{group.Count()} {group.Key:yyyy-MM-dd}");
+                    }
+
+                    break;
+
+                case Mode.CommitAuthor:
+                    // group all commits by author
+                    var commitsByAuthorAndDate = repo.Commits
+                        .GroupBy(c => c.Author.Name);
+
+                    // show stats for each author
+                    foreach (var group in commitsByAuthorAndDate)
+                    {
+                        
+                        Console.WriteLine($"{group.Key} | {group.Count()} commits");
+                        
+                        // sort our authors commits into a grouping by date
+                        var authorSorted = group.AsEnumerable()
+                            .GroupBy(c => c.Author.When.Date)
+                            .OrderBy(g => g.Key);
+                        
+                        foreach (var grouping in authorSorted)
+                        {
+                            Console.WriteLine($"      {grouping.Count()} {grouping.Key:yyyy-MM-dd}");
+                        }
+                    }
+
+                    break;
+
+                default:
+                    Console.Error.WriteLine("Invalid mode");
+                    break;
+            }
+        }
+
+        // Parse the command-line arguments and return the mode
+        static Mode ParseArgs(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("Missing repository path");
+                Environment.Exit(1);
+            }
+
+            if (args.Length < 2)
+            {
+                return Mode.CommitFrequency;
+            }
+
+            switch (args[1])
+            {
+                case "-a":
+                case "--author":
+                    return Mode.CommitAuthor;
+                default:
+                    return Mode.Unknown;
+            }
+        }
+    }
+}
---
+++ GitInsight/GitInsight/docs/README.md
@@ -0,0 +1,6 @@
+﻿# Functional requirements
+I've identified the following functional requirements
+1. The application should be able to collect all commits with respective author names and author dates from a Git repository that resides in a local directory.
+2. The application should be able to run in two modes: commit frequency mode and commit author mode. 
+3. In commit frequency mode, the application should produce textual output on stdout that lists the number of commits per day. 
+4. In commit author mode, the application should produce textual output on stdout that lists the number of commits per day per author.
